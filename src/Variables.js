1d41b82db11c006b2890ca737265e90beed6dede
SinkLine_P <popovartem505@gmail.com>
2023-10-04 08:38:17 -0400

    fixed colorize header status

--- src/Variables.js
+++ src/Variables.js
@@ -2,7 +2,8 @@ export const StatusesColors = {
   Queue: "#eba946",
   Development: "#00C2E0",
   Done: "#70a138",
-  Height: "#f36464"
+  Height: "#f36464",
+  Default: "#a1a1a1"
 }
 
 
--- src/components/ColorizeWrapText/ColorizeWrapText.js
+++ src/components/ColorizeWrapText/ColorizeWrapText.js
@@ -6,15 +6,16 @@ import "./ColorizeWrapText.css";
 export default function ColorizeWrapText({ text, label, type }) {
     const StatusColor = (status) => {
         if (status.toLowerCase() === "height") {
-            return StatusesColors.Height
-        }
-
-        if (status.toLowerCase() === "queue" || "low") {
+            return StatusesColors.Height;
+        } else if (status.toLowerCase() === "queue" || status.toLowerCase() === "low") {
             return StatusesColors.Queue;
-        } else if (status.toLowerCase() === "development" || "medium") {
-            return StatusesColors.Development
+        } else if (status.toLowerCase() === "development" || status.toLowerCase() === "medium") {
+            return StatusesColors.Development;
         } else if (status.toLowerCase() === "done") {
-            return StatusesColors.Done
+            return StatusesColors.Done;
+        } else {
+            // Если статус не совпадает ни с одним из вышеперечисленных, вернуть значение по умолчанию или undefined.
+            return StatusesColors.Default; // или StatusesColors.Default или что-то ещё по вашему выбору
         }
     }
 
